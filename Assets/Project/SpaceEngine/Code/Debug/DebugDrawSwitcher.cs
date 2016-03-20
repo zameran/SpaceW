ad43e9f951dbb8311f797c279a5692b51b5cacb8
Овчинников Денис Андреевич <zameran9@gmail.com>
2016-03-20 12:21:29 +0300

    One More switcher

---
+++ Assets/Project/SpaceEngine/Code/Debug/DebugDrawSwitcher.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class DebugDrawSwitcher : MonoBehaviour
+{
+    public DebugDraw[] GUIs;
+
+    public int state = 0;
+
+    private void Start()
+    {
+        ToogleAll(GUIs, false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F6))
+        {
+            if (state == GUIs.Length)
+            {
+                state = 0;
+                ToogleAll(GUIs, false);
+                return;
+            }
+
+            ToogleAll(GUIs, false);
+            state++;
+            ToogleAt(GUIs, true, state);
+        }
+    }
+
+    public void Toogle(DebugDraw GUI, bool state)
+    {
+        GUI.enabled = state;
+    }
+
+    public void ToogleAt(DebugDraw[] GUIs, bool state, int index)
+    {
+        GUIs[index - 1].enabled = state;
+    }
+
+    public void ToogleAll(DebugDraw[] GUIs, bool state)
+    {
+        for (int i = 0; i < GUIs.Length; i++)
+        {
+            GUIs[i].enabled = false;
+        }
+    }
+}
