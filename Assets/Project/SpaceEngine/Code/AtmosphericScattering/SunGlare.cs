0c865fe6f1faa9856f281279645d20643b2eb3e3
Овчинников Денис Андреевич <zameran9@gmail.com>
2016-05-05 12:28:12 +0300

    Sunglare begin.

--- Assets/Project/SpaceEngine/Code/AtmosphericScattering/SunGlare.cs
+++ Assets/Project/SpaceEngine/Code/AtmosphericScattering/SunGlare.cs
@@ -1,143 +1,95 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.IO;
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
 
 public sealed class SunGlare : MonoBehaviour
 {
-    /*
+    public Atmosphere Atmosphere;
+
     public Shader sunglareShader;
-    public Material sunglareMaterial;
+    private Material sunglareMaterial;
 
-    Texture2D sunSpikes;
-    Texture2D sunFlare;
-    Texture2D sunGhost1;
-    Texture2D sunGhost2;
+    public Texture2D sunSpikes;
+    public Texture2D sunFlare;
+    public Texture2D sunGhost1;
+    public Texture2D sunGhost2;
 
-    bool transmittanceSet = false;
+    private bool eclipse = false;
 
-    Vector3 sunViewPortPos = Vector3.zero;
+    private Vector3 sunViewPortPos = Vector3.zero;
 
-    RaycastHit hit;
-    bool hitStatus = false;
-    bool eclipse = false;
-
-    float sunGlareScale = 1;
+    private float sunGlareScale = 1;
 
     Mesh screenMesh;
 
-    Vector3 flareSettings = Vector3.zero;
-    Vector3 spikesSettings = Vector3.zero;
+    public Vector3 flareSettings = new Vector3(0.45f, 1.0f, 0.85f);
+    public Vector3 spikesSettings = new Vector3(0.6f, 1.0f, 1.0f);
 
-    List<Vector4> ghost1SettingsList = new List<Vector4> { };
-    List<Vector4> ghost2SettingsList = new List<Vector4> { };
+    public List<Vector4> ghost1SettingsList = new List<Vector4> { new Vector4(0.54f, 0.65f, 2.3f, 0.5f), new Vector4(0.54f, 1.0f, 6.0f, 0.7f) };
+    public List<Vector4> ghost2SettingsList = new List<Vector4> { new Vector4(0.135f, 1.0f, 3.0f, 0.9f), new Vector4(0.054f, 1.0f, 8.0f, 1.1f), new Vector4(0.054f, 1.0f, 4.0f, 1.3f), new Vector4(0.054f, 1.0f, 5.0f, 1.5f) };
 
-
-    public void start()
+    public void Start()
     {
         sunglareMaterial = new Material(sunglareShader);
-
-        sunSpikes = new Texture2D(1, 1);
-        sunFlare = new Texture2D(1, 1);
-        sunGhost1 = new Texture2D(1, 1);
-        sunGhost2 = new Texture2D(1, 1);
 
         sunglareMaterial.SetTexture("sunSpikes", sunSpikes);
         sunglareMaterial.SetTexture("sunFlare", sunFlare);
         sunglareMaterial.SetTexture("sunGhost1", sunGhost1);
         sunglareMaterial.SetTexture("sunGhost2", sunGhost2);
-        //sunglareMaterial.SetTexture ("_customDepthTexture", inCore.customDepthBufferTexture);
 
         sunglareMaterial.renderQueue = 3000;
 
         screenMesh = MeshFactory.MakePlane(2, 2, MeshFactory.PLANE.XY, false, false, false);
-        screenMesh.bounds = new Bounds(Vector4.zero, new Vector3(Mathf.Infinity, Mathf.Infinity, Mathf.Infinity));
+        screenMesh.bounds = new Bounds(Vector4.zero, new Vector3(9e37f, 9e37f, 9e37f));
 
-        //didn't want to serialize the matrices directly as the result is pretty unreadable
         Matrix4x4 ghost1Settings = Matrix4x4.zero;
-        for (int i = 0; i < ghost1SettingsList.Count; i++)
-        {
-            ghost1Settings.SetRow(i, ghost1SettingsList[i]);
-        }
-
         Matrix4x4 ghost2Settings = Matrix4x4.zero;
+
+        for (int i = 0; i < ghost1SettingsList.Count; i++)
+            ghost1Settings.SetRow(i, ghost1SettingsList[i]);
+
         for (int i = 0; i < ghost2SettingsList.Count; i++)
-        {
             ghost2Settings.SetRow(i, ghost2SettingsList[i]);
-        }
 
         sunglareMaterial.SetVector("flareSettings", flareSettings);
         sunglareMaterial.SetVector("spikesSettings", spikesSettings);
         sunglareMaterial.SetMatrix("ghost1Settings", ghost1Settings);
         sunglareMaterial.SetMatrix("ghost2Settings", ghost2Settings);
+
+        if (Atmosphere != null) Atmosphere.InitUniforms(sunglareMaterial);
     }
 
-    public void OnPreRender()
+    public void UpdateNode()
     {
-        sunViewPortPos = inCore.scaledSpaceCamera.WorldToViewportPoint
-            (ScaledSpace.LocalToScaledSpace(inCore.sunCelestialBody.transform.position));
+        if (Atmosphere == null) return;
 
-        sunGlareScale = (float)(inCore.scaledSpaceCamera.transform.position - ScaledSpace.LocalToScaledSpace(inCore.sunCelestialBody.transform.position))
-            .magnitude / 2266660f;
+        RaycastHit hit;
 
-        hitStatus = false;
+        sunViewPortPos = Camera.main.WorldToViewportPoint(Atmosphere.Sun_1.transform.position);
+        sunGlareScale = (Camera.main.transform.position - Atmosphere.Sun_1.transform.position).magnitude / 2266660f;
 
-        hitStatus = Physics.Raycast(inCore.farCamera.transform.position,
-                                     (inCore.sunCelestialBody.transform.position - inCore.farCamera.transform.position).normalized,
-                                     out hit, Mathf.Infinity, (int)((1 << 15) + (1 << 0)));
+        eclipse = false;
+        eclipse = Physics.Raycast(Camera.main.transform.position, (Atmosphere.Sun_1.transform.position - Camera.main.transform.position).normalized, out hit, Mathf.Infinity);
 
-        if (!hitStatus)
-        {
-            hitStatus = Physics.Raycast(inCore.scaledSpaceCamera.transform.position,
-                                         (ScaledSpace.LocalToScaledSpace(inCore.sunCelestialBody.transform.position) - inCore.scaledSpaceCamera.transform.position)
-                                         .normalized, out hit, Mathf.Infinity, (int)((1 << 10)));
-        }
-
-
-        eclipse = hitStatus;
+        if (!eclipse)
+            eclipse = Physics.Raycast(Camera.main.transform.position, (Atmosphere.Sun_1.transform.position - Camera.main.transform.position).normalized, out hit, Mathf.Infinity);
 
         sunglareMaterial.SetVector("sunViewPortPos", sunViewPortPos);
-        sunglareMaterial.SetFloat("aspectRatio", inCore.nearCamera.aspect);
+        sunglareMaterial.SetFloat("aspectRatio", Camera.main.aspect);
         sunglareMaterial.SetFloat("sunGlareScale", sunGlareScale);
+        sunglareMaterial.SetFloat("useTransmittance", 1f);
 
-        if (!transmittanceSet)
-        {
-            sunglareMaterial.SetFloat("Rg", inCore.managerWactivePQS.m_skyNode.Rg);
-            sunglareMaterial.SetFloat("Rt", inCore.managerWactivePQS.m_skyNode.Rt);
-            sunglareMaterial.SetTexture("_Sky_Transmittance", inCore.managerWactivePQS.m_skyNode.m_transmit);
-
-            sunglareMaterial.SetFloat("useTransmittance", 1f);
-            transmittanceSet = true;
-        }
-
-        if (transmittanceSet)
-        {
-            if (!inCore.pqsEnabled)
-            {
-                sunglareMaterial.SetFloat("useTransmittance", 0f);
-                transmittanceSet = false;
-            }
-            else
-            {
-                sunglareMaterial.SetVector("_Globals_WorldCameraPos", inCore.farCamera.transform.position - lastActivePQS.parentCelestialBody.transform.position);
-                sunglareMaterial.SetVector("_Sun_WorldSunDir", lastActivePQS.getDirectionToSun().normalized);
-            }
-        }
+        Atmosphere.SetUniforms(sunglareMaterial);
     }
 
-    public void updateNode()
+    public void Update()
     {
+        UpdateNode();
+
         if (!eclipse && (sunViewPortPos.z > 0))
         {
-            Graphics.DrawMesh(screenMesh, Vector3.zero, Quaternion.identity, sunglareMaterial, 10,
-                               inCore.scaledSpaceCamera, 0, null, false, false);
-
+            Graphics.DrawMesh(screenMesh, Vector3.zero, Quaternion.identity, sunglareMaterial, 10, Camera.main, 0, null, false, false);
         }
     }
-    */
 }
