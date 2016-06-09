247a66ff72e54085b741469fad4f7616755b411f
Овчинников Денис Андреевич <zameran9@gmail.com>
2016-06-09 19:36:43 +0300

    Another shit.
    Former-commit-id: 2de5157f82c6269097cb2e533d786c848cfbaa26

---
+++ Assets/Project/SpaceEngine/Code/Tools/Yielders.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+public static class Yielders
+{
+    class FloatComparer : IEqualityComparer<float>
+    {
+        bool IEqualityComparer<float>.Equals(float x, float y)
+        {
+            return x == y;
+        }
+
+        int IEqualityComparer<float>.GetHashCode(float obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+
+    static Dictionary<float, WaitForSeconds> _timeInterval = new Dictionary<float, WaitForSeconds>(100, new FloatComparer());
+
+    static WaitForEndOfFrame _endOfFrame = new WaitForEndOfFrame();
+
+    public static WaitForEndOfFrame EndOfFrame
+    {
+        get { return _endOfFrame; }
+    }
+
+    static WaitForFixedUpdate _fixedUpdate = new WaitForFixedUpdate();
+
+    public static WaitForFixedUpdate FixedUpdate
+    {
+        get { return _fixedUpdate; }
+    }
+
+    public static WaitForSeconds Get(float seconds)
+    {
+        WaitForSeconds wfs;
+
+        if (!_timeInterval.TryGetValue(seconds, out wfs))
+            _timeInterval.Add(seconds, wfs = new WaitForSeconds(seconds));
+
+        return wfs;
+    }
+}
