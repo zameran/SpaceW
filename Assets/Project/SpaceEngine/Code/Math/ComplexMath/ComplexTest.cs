b4166bba16ff1ec00dab01bf4dc78e44d80149c5
Овчинников Денис Андреевич <zameran9@gmail.com>
2016-06-25 17:33:34 +0300

    Complex, ComplexVector, complex stuff.
    Former-commit-id: 2cf0f1d8c7164b6b02fd2094c3cfddf0a6a960e0

---
+++ Assets/Project/SpaceEngine/Code/Math/ComplexMath/ComplexTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ComplexTest : MonoBehaviour
+{
+    public Complex a = new Complex(0, 100);
+    public Complex b = new Complex(1, 50);
+    public ComplexVector v = ComplexVector.Zero;
+
+    private void Start()
+    {
+        Debug.Log("A: " + a.ToString());
+        Debug.Log("B: " + b.ToString());
+        Debug.Log("A + B: " + (a + b).ToString());
+
+        Debug.Log("V: " + v.ToString());
+    }
+}
---
+++ Assets/Project/SpaceEngine/Code/Math/ComplexMath/ComplexVector.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Globalization;
+
+namespace UnityEngine
+{
+    [Serializable]
+    public struct ComplexVector : IEquatable<ComplexVector>
+    {
+        public Complex x;
+        public Complex y;
+        public Complex z;
+
+        public ComplexVector(Complex x)
+        {
+            this.x = x;
+            this.y = Complex.Zero;
+            this.z = Complex.Zero;
+        }
+
+        public ComplexVector(Complex x, Complex y)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = Complex.Zero;
+        }
+
+        public ComplexVector(Complex x, Complex y, Complex z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static readonly ComplexVector Zero = new ComplexVector(Complex.Zero, Complex.Zero, Complex.Zero);
+        public static readonly ComplexVector One = new ComplexVector(Complex.One, Complex.One, Complex.One);
+
+        public static ComplexVector operator +(ComplexVector left, ComplexVector right)
+        {
+            return new ComplexVector(left.x + right.x, left.y + right.y, left.z + left.z);
+        }
+
+        public static ComplexVector operator -(ComplexVector left, ComplexVector right)
+        {
+            return new ComplexVector(left.x - right.x, left.y - right.y, left.z - right.z);
+        }
+
+        public static ComplexVector operator -(ComplexVector left)
+        {
+            return new ComplexVector(-left.x, -left.y, -left.z);
+        }
+
+        public static ComplexVector operator *(ComplexVector left, ComplexVector right)
+        {
+            return new ComplexVector(left.x * right.x, left.y * right.y, left.z * right.z);
+        }
+
+        public static ComplexVector operator *(ComplexVector left, Complex right)
+        {
+            return new ComplexVector(left.x * right, left.y * right, left.x * right);
+        }
+
+        public static ComplexVector operator *(ComplexVector left, double right)
+        {
+            return new ComplexVector(left.x * right, left.y * right, left.z * right);
+        }
+
+        public static ComplexVector operator /(ComplexVector left, ComplexVector right)
+        {
+            return new ComplexVector(left.x / right.x, left.y / right.y, left.z / right.z);
+        }
+
+        public static ComplexVector operator /(ComplexVector left, Complex right)
+        {
+            return new ComplexVector(left.x / right, left.y / right, left.z / right);
+        }
+
+        public static ComplexVector operator /(ComplexVector left, double right)
+        {
+            return new ComplexVector(left.x / right, left.y / right, left.z / right);
+        }
+
+        public static bool operator ==(ComplexVector left, ComplexVector right)
+        {
+            return (left.x == right.x && left.y == right.y && left.z == right.z);
+        }
+
+        public static bool operator !=(ComplexVector left, ComplexVector right)
+        {
+            return (left.x != right.x || left.y != right.y || left.z != right.z);
+        }
+
+        public override int GetHashCode()
+        {
+            return (x.GetHashCode() ^ y.GetHashCode()) << 2 + z.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is ComplexVector)) return false;
+
+            return this == (ComplexVector)obj;
+        }
+
+        public bool Equals(ComplexVector value)
+        {
+            return (this.x.Equals(value.x) && this.y.Equals(value.y) && this.z.Equals(value.z));
+        }
+
+        public override string ToString()
+        {
+            return (string.Format(CultureInfo.CurrentCulture, "({0}, {1}, {2})", this.x.ToString(), this.y.ToString(), this.z.ToString()));
+        }
+    }
+}
