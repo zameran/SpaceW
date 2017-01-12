d4008e256cc40107dc08b5a1e100088ab52be129
Овчинников Денис Андреевич <zameran9@gmail.com>
2017-01-12 13:55:43 +0300

    Begin 2. Make it generic.
    Former-commit-id: 41c1ef81b60c03a82249131eba00a47891b4bc30

--- Assets/Project/SpaceEngine/Code/Spaces/SpaceGeneric.cs
+++ Assets/Project/SpaceEngine/Code/Spaces/SpaceGeneric.cs
@@ -33,19 +33,133 @@
 // Creator: zameran
 #endregion
 
+using System;
+
 using UnityEngine;
 
-public class SpaceGeneric<TChildType> : Space where TChildType : Space
+public abstract class SpaceGeneric
+{
+    public Guid GUID { get; private set; }
+
+    public Vector3d Position { get; protected set; }
+
+    public SpaceGeneric Parent;
+    public SpaceGeneric Current;
+
+    public abstract byte Size { get; }
+    public abstract long SpaceSize { get; }
+    public byte HalfSize { get { return (byte)(Size / 2); } }
+    public long SideSize { get { return Size * SpaceSize; } }
+    public long HalfSpaceSize { get { return SpaceSize / 2; } }
+    public Vector3d Shift { get { return new Vector3d(HalfSpaceSize, HalfSpaceSize, HalfSpaceSize); } }
+
+    protected SpaceGeneric()
+    {
+        this.GUID = Guid.NewGuid();
+
+        this.Position = Vector3d.zero;
+    }
+
+    protected SpaceGeneric(Vector3d Position)
+    {
+        this.GUID = Guid.NewGuid();
+
+        this.Position = Position;
+    }
+
+    protected void UpdateFromParent(Vector3d Position)
+    {
+        if (Parent == null) return;
+
+        Update(Position);
+    }
+
+    public abstract void Init();
+
+    public abstract void Update(Vector3d Position);
+
+    public abstract void DrawDebug();
+}
+
+public class SpaceGeneric<TChildType> : SpaceGeneric where TChildType : SpaceGeneric, new()
 {
     public TChildType[,,] LeafNodes;
 
+    public override byte Size { get { return 8; } }
+    public override long SpaceSize { get { return 65536; } }
+
     public override void Init()
     {
+        LeafNodes = new TChildType[Size, Size, Size];
 
+        for (var x = -HalfSize; x < HalfSize; x++)
+        {
+            for (var y = -HalfSize; y < HalfSize; y++)
+            {
+                for (var z = -HalfSize; z < HalfSize; z++)
+                {
+                    var idx = x + HalfSize;
+                    var idy = y + HalfSize;
+                    var idz = z + HalfSize;
+
+                    var position = new Vector3d(x * SpaceSize, y * SpaceSize, z * SpaceSize) + Shift;
+                    var node = new TChildType();
+
+                    node.Parent = this;
+                    node.Init();
+                    node.Update(Position + position);
+
+                    LeafNodes[idx, idy, idz] = node;
+                }
+            }
+        }
     }
 
     public override void Update(Vector3d Position)
     {
+        if (LeafNodes != null)
+        {
+            for (var x = 0; x < Size; x++)
+            {
+                for (var y = 0; y < Size; y++)
+                {
+                    for (var z = 0; z < Size; z++)
+                    {
+                        var node = LeafNodes[x, y, z];
 
+                        if (node != null)
+                        {
+                            node.Update(Position + (node.Position - this.Position));
+                        }
+                    }
+                }
+            }
+        }
+
+        this.Position = Position;
+    }
+
+    public override void DrawDebug()
+    {
+        if (LeafNodes != null)
+        {
+            var size = Vector3.one * SpaceSize;
+
+            for (var x = 0; x < Size; x++)
+            {
+                for (var y = 0; y < Size; y++)
+                {
+                    for (var z = 0; z < Size; z++)
+                    {
+                        var node = LeafNodes[x, y, z];
+
+                        if (node != null)
+                        {
+                            Gizmos.DrawWireCube(node.Position, size);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
