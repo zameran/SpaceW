9a244e7b64a7d61c28f6814d7d88f6738152b6d0
Овчинников Денис Андреевич <zameran9@gmail.com>
2017-01-25 11:01:13 +0300

    Put sunglare settings in to the scriptable object.

--- Assets/Project/SpaceEngine/Code/Enviroment/Sun/SunGlare.cs
+++ Assets/Project/SpaceEngine/Code/Enviroment/Sun/SunGlare.cs
@@ -33,7 +33,6 @@
 // Creator: zameran
 #endregion
 
-using System.Collections.Generic;
 
 using UnityEngine;
 
@@ -50,11 +49,7 @@ namespace SpaceEngine.AtmosphericScattering.Sun
         public Shader SunGlareShader;
         private Material SunGlareMaterial;
 
-        public Texture2D SunSpikes;
-        public Texture2D SunFlare;
-        public Texture2D SunGhost1;
-        public Texture2D SunGhost2;
-        public Texture2D SunGhost3;
+        public SunGlareSettings Settings;
 
         public EngineRenderQueue RenderQueue = EngineRenderQueue.Transparent;
         public int RenderQueueOffset = 1000000; //NOTE : Render over all.
@@ -76,31 +71,6 @@ namespace SpaceEngine.AtmosphericScattering.Sun
 
         private Mesh SunGlareMesh;
 
-        public Vector3 FlareSettings = new Vector3(0.45f, 1.0f, 0.85f);
-        public Vector3 SpikesSettings = new Vector3(0.6f, 1.0f, 1.0f);
-
-        public List<Vector4> Ghost1SettingsList = new List<Vector4>
-        {
-            new Vector4(0.54f, 0.65f, 2.3f, 0.5f),
-            new Vector4(0.54f, 1.0f, 6.0f, 0.7f)
-        };
-
-        public List<Vector4> Ghost2SettingsList = new List<Vector4>
-        {
-            new Vector4(0.135f, 1.0f, 3.0f, 0.9f),
-            new Vector4(0.054f, 1.0f, 8.0f, 1.1f),
-            new Vector4(0.054f, 1.0f, 4.0f, 1.3f),
-            new Vector4(0.054f, 1.0f, 5.0f, 1.5f)
-        };
-
-        public List<Vector4> Ghost3SettingsList = new List<Vector4>
-        {
-            new Vector4(0.135f, 1.0f, 3.0f, 0.9f),
-            new Vector4(0.054f, 1.0f, 8.0f, 1.1f),
-            new Vector4(0.054f, 1.0f, 4.0f, 1.3f),
-            new Vector4(0.054f, 1.0f, 5.0f, 1.5f)
-        };
-
         private Matrix4x4 Ghost1Settings = Matrix4x4.zero;
         private Matrix4x4 Ghost2Settings = Matrix4x4.zero;
         private Matrix4x4 Ghost3Settings = Matrix4x4.zero;
@@ -109,25 +79,29 @@ namespace SpaceEngine.AtmosphericScattering.Sun
 
         protected override void InitNode()
         {
+            if (Settings == null) return;
+
             SunGlareMaterial = MaterialHelper.CreateTemp(SunGlareShader, "Sunglare", (int)RenderQueue);
 
             SunGlareMesh = MeshFactory.MakePlane(8, 8, MeshFactory.PLANE.XY, false, false, false);
             SunGlareMesh.bounds = new Bounds(Vector4.zero, new Vector3(9e37f, 9e37f, 9e37f));
 
-            for (int i = 0; i < Ghost1SettingsList.Count; i++)
-                Ghost1Settings.SetRow(i, Ghost1SettingsList[i]);
+            for (int i = 0; i < Settings.Ghost1SettingsList.Count; i++)
+                Ghost1Settings.SetRow(i, Settings.Ghost1SettingsList[i]);
 
-            for (int i = 0; i < Ghost2SettingsList.Count; i++)
-                Ghost2Settings.SetRow(i, Ghost2SettingsList[i]);
+            for (int i = 0; i < Settings.Ghost2SettingsList.Count; i++)
+                Ghost2Settings.SetRow(i, Settings.Ghost2SettingsList[i]);
 
-            for (int i = 0; i < Ghost3SettingsList.Count; i++)
-                Ghost3Settings.SetRow(i, Ghost3SettingsList[i]);
+            for (int i = 0; i < Settings.Ghost3SettingsList.Count; i++)
+                Ghost3Settings.SetRow(i, Settings.Ghost3SettingsList[i]);
 
             InitUniforms(SunGlareMaterial);
         }
 
         protected override void UpdateNode()
         {
+            if (Settings == null) return;
+
             SunGlareMaterial.renderQueue = (int)RenderQueue + RenderQueueOffset;
 
             var distance = (CameraHelper.Main().transform.position.normalized - SunComponent.transform.position.normalized).magnitude;
@@ -190,14 +164,14 @@ namespace SpaceEngine.AtmosphericScattering.Sun
         {
             if (mat == null) return;
 
-            SunGlareMaterial.SetTexture("sunSpikes", SunSpikes);
-            SunGlareMaterial.SetTexture("sunFlare", SunFlare);
-            SunGlareMaterial.SetTexture("sunGhost1", SunGhost1);
-            SunGlareMaterial.SetTexture("sunGhost2", SunGhost2);
-            SunGlareMaterial.SetTexture("sunGhost3", SunGhost3);
+            SunGlareMaterial.SetTexture("sunSpikes", Settings.SunSpikes);
+            SunGlareMaterial.SetTexture("sunFlare", Settings.SunFlare);
+            SunGlareMaterial.SetTexture("sunGhost1", Settings.SunGhost1);
+            SunGlareMaterial.SetTexture("sunGhost2", Settings.SunGhost2);
+            SunGlareMaterial.SetTexture("sunGhost3", Settings.SunGhost3);
 
-            SunGlareMaterial.SetVector("flareSettings", FlareSettings);
-            SunGlareMaterial.SetVector("spikesSettings", SpikesSettings);
+            SunGlareMaterial.SetVector("flareSettings", Settings.FlareSettings);
+            SunGlareMaterial.SetVector("spikesSettings", Settings.SpikesSettings);
             SunGlareMaterial.SetMatrix("ghost1Settings", Ghost1Settings);
             SunGlareMaterial.SetMatrix("ghost2Settings", Ghost2Settings);
             SunGlareMaterial.SetMatrix("ghost3Settings", Ghost2Settings);
---
+++ Assets/Project/SpaceEngine/Code/SciptableObjects/SunGlareSettings.cs
@@ -0,0 +1,77 @@
+﻿#region License
+// Procedural planet generator.
+//  
+// Copyright (C) 2015-2017 Denis Ovchinnikov [zameran] 
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. Neither the name of the copyright holders nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION)HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+// THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// Creation Date: 2017.01.25
+// Creation Time: 10:46 AM
+// Creator: zameran
+#endregion
+
+using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenuAttribute(fileName = "SunGlareSettings", menuName = "Create SunGlare Settings")]
+public class SunGlareSettings : ScriptableObject
+{
+    [Header("Textures")]
+    public Texture2D SunSpikes;
+    public Texture2D SunFlare;
+    public Texture2D SunGhost1;
+    public Texture2D SunGhost2;
+    public Texture2D SunGhost3;
+
+    [Header("Settings")]
+    public Vector3 FlareSettings = new Vector3(0.45f, 1.0f, 0.85f);
+    public Vector3 SpikesSettings = new Vector3(0.6f, 1.0f, 1.0f);
+
+    public List<Vector4> Ghost1SettingsList = new List<Vector4>
+    {
+        new Vector4(0.54f, 0.65f, 2.3f, 0.5f),
+        new Vector4(0.54f, 1.0f, 6.0f, 0.7f)
+    };
+
+    public List<Vector4> Ghost2SettingsList = new List<Vector4>
+    {
+        new Vector4(0.135f, 1.0f, 3.0f, 0.9f),
+        new Vector4(0.054f, 1.0f, 8.0f, 1.1f),
+        new Vector4(0.054f, 1.0f, 4.0f, 1.3f),
+        new Vector4(0.054f, 1.0f, 5.0f, 1.5f)
+    };
+
+    public List<Vector4> Ghost3SettingsList = new List<Vector4>
+    {
+        new Vector4(0.135f, 1.0f, 3.0f, 0.9f),
+        new Vector4(0.054f, 1.0f, 8.0f, 1.1f),
+        new Vector4(0.054f, 1.0f, 4.0f, 1.3f),
+        new Vector4(0.054f, 1.0f, 5.0f, 1.5f)
+    };
+}
