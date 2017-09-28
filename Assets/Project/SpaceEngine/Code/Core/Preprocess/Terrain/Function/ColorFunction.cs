1d86385779624eacc9e379e9763af8f031577f14
Овчинников Денис Андреевич <zameran9@gmail.com>
2017-09-28 12:03:40 +0300

    Get ready for a spherical terrain preprocess.

--- Assets/Project/SpaceEngine/Code/Core/Preprocess/Terrain/Function/ColorFunction.cs
+++ Assets/Project/SpaceEngine/Code/Core/Preprocess/Terrain/Function/ColorFunction.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+
+using UnityEngine;
 
 namespace SpaceEngine.Core.Preprocess.Terrain
 {
@@ -19,7 +21,7 @@ namespace SpaceEngine.Core.Preprocess.Terrain
             return SampleColor((float)x / (float)DestinationSize * (float)Source.Width, (float)y / (float)DestinationSize * (float)Source.Height);
         }
 
-        Vector4 SampleColor(float x, float y)
+        private Vector4 SampleColor(float x, float y)
         {
             int ix = (int)Mathf.Floor(x);
             int iy = (int)Mathf.Floor(y);
@@ -41,6 +43,64 @@ namespace SpaceEngine.Core.Preprocess.Terrain
                 (c1.y * cx + c2.y * x) * cy + (c3.y * cx + c4.y * x) * y,
                 (c1.z * cx + c2.z * x) * cy + (c3.z * cx + c4.z * x) * y,
                 (c1.w * cx + c2.w * x) * cy + (c3.w * cx + c4.w * x) * y
+            );
+
+            return color;
+        }
+    }
+
+    public class SphericalColorFunction : IColorFunction2D
+    {
+        InputMap Source;
+
+        Projection Projection;
+
+        int DestinationSize;
+
+        public SphericalColorFunction(InputMap source, Projection projection, int destinationSize)
+        {
+            this.Source = source;
+            this.Projection = projection;
+            this.DestinationSize = destinationSize;
+        }
+
+        public Vector4 GetValue(int x, int y)
+        {
+            double sx, sy, sz;
+
+            Projection(x, y, DestinationSize, out sx, out sy, out sz);
+
+            double lon = Math.Atan2(sy, sx) + Math.PI;
+            double lat = Math.Acos(sz);
+
+            return SampleColor(lon, lat);
+        }
+
+        private Vector4 SampleColor(double lon, double lat)
+        {
+            lon = lon / Math.PI * (Source.Width / 2.0);
+            lat = lat / Math.PI * Source.Height;
+
+            int ilon = (int)Math.Floor(lon);
+            int ilat = (int)Math.Floor(lat);
+
+            lon -= ilon;
+            lat -= ilat;
+
+            double clon = 1.0 - lon;
+            double clat = 1.0 - lat;
+
+            var c1 = Source.Get((ilon + Source.Width) % Source.Width, ilat);
+            var c2 = Source.Get((ilon + Source.Width + 1) % Source.Width, ilat);
+            var c3 = Source.Get((ilon + Source.Width) % Source.Width, ilat + 1);
+            var c4 = Source.Get((ilon + Source.Width + 1) % Source.Width, ilat + 1);
+
+            var color = new Vector4
+            (
+                (float)((c1.x * clon + c2.x * lon) * clat + (c3.x * clon + c4.x * lon) * lat),
+                (float)((c1.y * clon + c2.y * lon) * clat + (c3.y * clon + c4.y * lon) * lat),
+                (float)((c1.z * clon + c2.z * lon) * clat + (c3.z * clon + c4.z * lon) * lat),
+                (float)((c1.w * clon + c2.w * lon) * clat + (c3.w * clon + c4.w * lon) * lat)
             );
 
             return color;
