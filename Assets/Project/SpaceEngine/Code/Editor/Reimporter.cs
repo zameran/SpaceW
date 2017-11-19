e501cefa88ae18cfce8f71ff59b3369dcca87449
Овчинников Денис Андреевич <zameran9@gmail.com>
2017-11-20 00:11:08 +0300

    Typo.

--- Assets/Project/SpaceEngine/Code/Editor/Reimporter.cs
+++ Assets/Project/SpaceEngine/Code/Editor/Reimporter.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class Reimporter
 {
-    [MenuItem("SpaceEngine/Reimport Folder _%#B")]
+    [MenuItem("SpaceEngine/Reimport Shaders in Folder _%#H")]
     public static void ReImportFolder()
     {
         var realPath = Application.dataPath;
@@ -13,14 +13,23 @@ public class Reimporter
         realPath = realPath.Remove(realPath.Length - 6);
 
         var selectedPath = realPath + AssetDatabase.GetAssetPath(Selection.activeObject);
-        var files = Directory.GetFiles(selectedPath, "*.compute", SearchOption.AllDirectories);
 
-        for (int i = 0; i < files.Length; i++)
+        ReimportFiles(realPath, selectedPath, ".compute");
+        ReimportFiles(realPath, selectedPath, ".shader");
+
+        AssetDatabase.Refresh();
+    }
+
+    private static void ReimportFiles(string dataPath, string path, string extension = ".asset")
+    {
+        var files = Directory.GetFiles(path, string.Format("*.{0}", extension), SearchOption.AllDirectories);
+
+        for (var i = 0; i < files.Length; i++)
         {
-            string file = files[i];
+            var file = files[i];
 
             file = file.Replace("\\", "/");
-            file = file.Remove(0, realPath.Length);
+            file = file.Remove(0, dataPath.Length);
 
             AssetDatabase.ImportAsset(file);
         }
