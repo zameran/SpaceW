6442e5c1f522bbb988867fe17dfd42e879818442
Овчинников Денис Андреевич <zameran9@gmail.com>
2017-12-06 23:00:00 +0300

    Uniforms Manager Logger usage.

--- Assets/Project/SpaceEngine/Code/Managers/UniformsManager.cs
+++ Assets/Project/SpaceEngine/Code/Managers/UniformsManager.cs
@@ -33,11 +33,15 @@
 // Creator: zameran
 #endregion
 
+using SpaceEngine.Core.Debugging;
 using SpaceEngine.Core.Patterns.Strategy.Uniformed;
 
 using UnityEngine;
 
+using Logger = SpaceEngine.Core.Debugging.Logger;
+
 [ExecutionOrder(-9997)]
+[UseLogger(LoggerCategory.Core)]
 public class UniformsManager : MonoSingleton<UniformsManager>, IUniformed, IUniformed<Material>, IUniformed<ComputeShader>
 {
     public Texture2D PermSampler = null;
@@ -45,7 +49,7 @@ public class UniformsManager : MonoSingleton<UniformsManager>, IUniformed, IUnif
     public Texture2D PermSamplerGL = null;
     public Texture2D PermGradSamplerGL = null;
     public Texture2D PlanetAtlas = null;
-    public Texture2D PlanetUV = null;
+    public Texture2D PlanetNoiseMap = null;
     public Texture2D PlanetColorMap = null;
 
     public Texture2D QuadTexture1 = null;
@@ -67,7 +71,7 @@ public class UniformsManager : MonoSingleton<UniformsManager>, IUniformed, IUnif
         Shader.SetGlobalTexture("AtlasDiffSampler", PlanetAtlas);
         Shader.SetGlobalTexture("PlanetColorMap", PlanetColorMap);
 
-        Shader.SetGlobalTexture("_PlanetUV", PlanetUV);
+        Shader.SetGlobalTexture("_PlanetNoiseMap", PlanetNoiseMap);
 
         Shader.SetGlobalTexture("_QuadTexture1", QuadTexture1);
         Shader.SetGlobalTexture("_QuadTexture2", QuadTexture2);
@@ -118,7 +122,7 @@ public class UniformsManager : MonoSingleton<UniformsManager>, IUniformed, IUnif
         target.SetTexture("AtlasDiffSampler", PlanetAtlas);
         target.SetTexture("PlanetColorMap", PlanetColorMap);
 
-        target.SetTexture("_PlanetUV", PlanetUV);
+        target.SetTexture("_PlanetNoiseMap", PlanetNoiseMap);
 
         target.SetTexture("_QuadTexture1", QuadTexture1);
         target.SetTexture("_QuadTexture2", QuadTexture2);
@@ -209,11 +213,16 @@ public class UniformsManager : MonoSingleton<UniformsManager>, IUniformed, IUnif
         if (PermGradSamplerGL == null) PermGradSamplerGL = LoadTextureFromResources("Noise/PerlinGrad2D_GL");
         if (PlanetAtlas == null) PlanetAtlas = LoadTextureFromResources("PlanetAtlas");
         if (PlanetColorMap == null) PlanetColorMap = LoadTextureFromResources("PlanetColorHumanityToTemp");
-        if (PlanetUV == null) PlanetUV = LoadTextureFromResources("UV");
+        if (PlanetNoiseMap == null) PlanetNoiseMap = LoadTextureFromResources("Terrain/DetailNormalmap");
     }
 
     public Texture2D LoadTextureFromResources(string textureName)
     {
-        return Resources.Load("Textures/" + textureName, typeof(Texture2D)) as Texture2D;
+        var path = "Textures/" + textureName;
+        var textureResource = Resources.Load(path, typeof(Texture2D)) as Texture2D;
+
+        if (textureResource == null) { Logger.Log(string.Format("UniformsManager.LoadTextureFromResources: Failed to load texture from {0}", path)); }
+
+        return textureResource;
     }
 }
