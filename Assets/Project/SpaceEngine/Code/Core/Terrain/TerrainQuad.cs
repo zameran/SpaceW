5ace1fdd88e4adc8778015542bc34bff2661810e
Овчинников Денис Андреевич <zameran9@gmail.com>
2017-12-30 02:33:47 +0300

    Minimum split level. Just in case.

--- Assets/Project/SpaceEngine/Code/Core/Terrain/TerrainQuad.cs
+++ Assets/Project/SpaceEngine/Code/Core/Terrain/TerrainQuad.cs
@@ -329,6 +329,7 @@ namespace SpaceEngine.Core.Terrain
             var ground = Owner.ParentBody.HeightZ;
             var distance = Owner.GetCameraDistance(LocalBox, Math.Max(0.0, ground));
 
+            //if ((Owner.SplitInvisibleQuads || Visibility != Frustum3d.VISIBILITY.INVISIBLE) && (distance < Length * Owner.SplitDistance && Level < Owner.MaxLevel) || (Level < 2))
             if ((Owner.SplitInvisibleQuads || Visibility != Frustum3d.VISIBILITY.INVISIBLE) && distance < Length * Owner.SplitDistance && Level < Owner.MaxLevel)
             {
                 if (IsLeaf) { Subdivide(); }
